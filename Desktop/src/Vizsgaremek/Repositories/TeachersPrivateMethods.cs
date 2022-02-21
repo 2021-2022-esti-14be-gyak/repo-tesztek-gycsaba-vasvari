327c105c85b3106eabd25671f8e3f7b99776485e
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-21 17:14:07 +0100

    A metódus kap egy tanárt akit fel kell venni

--- Desktop/src/Vizsgaremek/Repositories/TeachersPrivateMethods.cs
+++ Desktop/src/Vizsgaremek/Repositories/TeachersPrivateMethods.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Vizsgaremek.Models;
+
 namespace Vizsgaremek.Repositories
 {
     public partial class Teachers
     {
-        private void InsertTeacherToTestData()
+        private void InsertTeacherToTestData(Teacher teacher)
         {
 
         }
